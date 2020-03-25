5cc0cc0e80fcc277fa9223f23a99f4d96b89c48b
Chase Gladish <chase.gladish@gmail.com>
2020-03-25 15:36:36 -0500

    Added Header component test

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { Header } from './Header';
+
+describe('Header', () => {
+    it('mounts and renders', () => {
+        const rendered = mount(<Header />);
+
+        expect(rendered.find('div').exists());
+    });
+});
