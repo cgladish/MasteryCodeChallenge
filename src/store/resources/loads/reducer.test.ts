b8b5b2e01dfe11426f7b6b7be175a2e1730bfbce
Chase Gladish <chase.gladish@gmail.com>
2020-03-25 00:41:39 -0500

    Finish tests for reducer file

--- src/store/resources/loads/reducer.test.ts
+++ src/store/resources/loads/reducer.test.ts
@@ -1,6 +1,7 @@
 import { ResourceStatus } from 'store/resources/types';
 import * as R from './reducer';
 import * as A from './actions';
+import { LoadsById } from './state';
 import { mockLoad, mockLoadsById } from './__mockData__';
 
 const fetchAction: A.FetchAction = { type: '@@loads/FETCH' };
@@ -31,7 +32,7 @@ describe('loads resource reducers', () => {
         ${modifyLoadStatusStartAction}   | ${'initial'} | ${'initial'}
         ${modifyLoadStatusSuccessAction} | ${'initial'} | ${'initial'}
     `(
-        'reduces fetchStatus for action $action.type from $initial to $expected',
+        'reduces fetchStatus for action $action.type',
         ({
             action,
             initial,
@@ -45,4 +46,67 @@ describe('loads resource reducers', () => {
             expect(newState).toEqual(expected);
         }
     );
+
+    it.each`
+        action                           | initial      | expected
+        ${fetchAction}                   | ${'initial'} | ${'initial'}
+        ${fetchStartAction}              | ${'initial'} | ${'initial'}
+        ${fetchSuccessAction}            | ${'initial'} | ${'initial'}
+        ${modifyLoadStatusAction}        | ${'initial'} | ${'initial'}
+        ${modifyLoadStatusStartAction}   | ${'initial'} | ${'pending'}
+        ${modifyLoadStatusSuccessAction} | ${'pending'} | ${'success'}
+    `(
+        'reduces modifyStatus for action $action.type',
+        ({
+            action,
+            initial,
+            expected,
+        }: {
+            action: A.Action;
+            initial: ResourceStatus;
+            expected: ResourceStatus;
+        }) => {
+            const newState = R.modifyStatus(initial, action);
+            expect(newState).toEqual(expected);
+        }
+    );
+
+    it.each`
+        action                           | initial | expected
+        ${fetchAction}                   | ${{}}   | ${{}}
+        ${fetchStartAction}              | ${{}}   | ${{}}
+        ${fetchSuccessAction}            | ${{}}   | ${mockLoadsById}
+        ${modifyLoadStatusAction}        | ${{}}   | ${{}}
+        ${modifyLoadStatusStartAction}   | ${{}}   | ${{}}
+        ${modifyLoadStatusSuccessAction} | ${{}}   | ${{}}
+    `(
+        'reduces loadsById for action $action.type',
+        ({
+            action,
+            initial,
+            expected,
+        }: {
+            action: A.Action;
+            initial: LoadsById;
+            expected: LoadsById;
+        }) => {
+            const newState = R.loadsById(initial, action);
+            expect(newState).toEqual(expected);
+        }
+    );
+
+    it('reduces loadsById for action @@loads/MODIFY_LOAD_STATUS_SUCCESS with existing state', () => {
+        const newState = R.loadsById(
+            mockLoadsById,
+            modifyLoadStatusSuccessAction
+        );
+        const expected = {
+            ...mockLoadsById,
+            [mockLoad.id]: {
+                ...mockLoad,
+                status: 'booked',
+            },
+        };
+        expect(newState).toEqual(expected);
+    });
 });
